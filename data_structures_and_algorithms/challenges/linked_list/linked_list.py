eb7ff7f82ba1fe9e703a15c2a1c65bbc7ba5183c
mohammedghafri <eng.m.ghafri@gmail.com>
2020-08-23 22:48:05 +0300

    0000

--- data_structures_and_algorithms/challenges/linked_list/linked_list.py
+++ data_structures_and_algorithms/challenges/linked_list/linked_list.py
@@ -60,6 +60,37 @@ class LinkedList():
 
             return ('This function Does not been established yet')
 
+    def insertAfter(self, prev_node, data):
+        if not prev_node:
+            print("not")
+            return
+        new_node = Node(data)
+
+        new_node.next = prev_node.next
+        prev_node.next = new_node
+
+    def insertBefore(self,data,new_data):
+        new_node=Node(new_data)
+        before=self.head
+        # if before==Node:
+        #     print("You should use pre_pend method !!!!")
+        #     return
+        old=before
+        while before:
+            if before.data==data:
+                new_node.next=before
+                old.next=new_node
+                return
+            else:
+                old=before
+            before=before.next
+                
+
+            
+
+
+
+
 
 
 
@@ -72,6 +103,9 @@ if __name__=='__main__':
     # bird.append_data("Eagle")
     bird.append_data("Dov")
     bird.append_data("Hawk")
+    bird.append_data("Haaaaawk")
+    bird.insertBefore("Eagle","before")
+    # bird.insertAfter("Hawk",'newOne')
     print(f""" "{bird}" """)
     # bird.includes("awk")
     bird.insert_btw("Hawk","Woody_Packer") # this for handle the error
