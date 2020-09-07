66ac7e0b1fc064b5a87261c3a3da50b553ff6ee6
mohammedghafri <eng.m.ghafri@gmail.com>
2020-09-07 23:48:22 +0300

    Breadth first

--- tests/challenges/test_tree.py
+++ tests/challenges/test_tree.py
@@ -118,6 +118,13 @@ def test_max_value(prepare_bt):
     actual=bt.find_maximum_value()
     assert expected==actual
     
+def test_breadth_first_1(prepare_for_breadth_first_1):
+    graph=prepare_for_breadth_first_1
+    bt=BinaryTree()
+    expected = "A -> B -> C -> D -> E -> F -> F ->  None"
+    actual=bt.breadth_first([],graph, 'A')
+    assert expected==actual
+
 
 
 @pytest.fixture
@@ -165,6 +172,18 @@ def prepare_bst_2():
     bst.add(0)
     bst.add(27)
     return bst
+
+@pytest.fixture
+def prepare_for_breadth_first_1():
+    graph = {
+    'A' : ['B','C'],
+    'B' : ['D', 'E'],
+    'C' : ['F'],
+    'D' : [],
+    'E' : ['F'],
+    'F' : []
+    }
+    return graph
 
 
 
